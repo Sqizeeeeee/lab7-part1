bc4d9db6a0939f19aba747c12bbf1a62fee960c5
vladimir <vladimir.oleynyk057@gmail.com>
2025-10-30 21:52:29 +0300

    added merge sort

--- src/algorithms/sorting.cpp
+++ src/algorithms/sorting.cpp
@@ -28,3 +28,50 @@ void bubble_sort(std::vector<int>& arr) {
         }
     }
 }
+
+// Вспомогательная функция для слияния
+void merge(const std::vector<int>& B, const std::vector<int>& C, std::vector<int>& A) {
+    int i = 0, j = 0, k = 0;
+    int p = B.size();
+    int q = C.size();
+    
+    while (i < p && j < q) {
+        if (B[i] <= C[j]) {
+            A[k] = B[i];
+            i = i + 1;
+        } else {
+            A[k] = C[j];
+            j = j + 1;
+        }
+        k = k + 1;
+    }
+    
+    if (i == p) {
+        // Копировать C[j..q-1] в A[k..p+q-1]
+        for (int idx = j; idx < q; idx++) {
+            A[k] = C[idx];
+            k = k + 1;
+        }
+    } else {
+        // Копировать B[i..p-1] в A[k..p+q-1]
+        for (int idx = i; idx < p; idx++) {
+            A[k] = B[idx];
+            k = k + 1;
+        }
+    }
+}
+
+// Сортировка слиянием
+void merge_sort(std::vector<int>& arr) {
+    int n = arr.size();
+    if (n > 1) {
+        // Копировать A[0..⌊n/2⌋-1] в B[0..⌊n/2⌋-1]
+        std::vector<int> B(arr.begin(), arr.begin() + n/2);
+        // Копировать A[⌊n/2⌋..n-1] в C[0..⌈n/2⌉-1]
+        std::vector<int> C(arr.begin() + n/2, arr.end());
+        
+        merge_sort(B);
+        merge_sort(C);
+        merge(B, C, arr);
+    }
+}
